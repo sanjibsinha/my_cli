80366452c2a17f656f87b1a973d5ba80edcde887
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-09-18 05:47:06 +0530

    second

--- bin/more_null_safety.dart
+++ bin/more_null_safety.dart
@@ -5,12 +5,16 @@ void main() {
   var provider = BarValueProvider();
   print('Getting value...');
   print('The value is ${provider.buzz}!');
+  print(provider.testCondition(false));
+  print(provider.testCondition(true));
 }
 
 /**
  * Calling constructor...
 Getting value...
 The value is 43!
+null
+1
  After you add late to the declaration of _cache, if you move the _computeValue function into the CachedValueProvider class the code still works! Initialization expressions 
  for late fields can use instance methods in their initializers.
  */
--- lib/more_null_safety.dart
+++ lib/more_null_safety.dart
@@ -3,10 +3,14 @@
 } */
 
 class BarValueProvider {
+  late final bool _cond = true;
+  bool get cond => _cond;
   int _returnValue() {
     return 43;
   }
 
   late final int _bar = _returnValue();
   int get buzz => _bar;
+
+  int? testCondition(cond) => cond ? 1 : null;
 }
